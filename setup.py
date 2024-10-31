ba776a60b1d21b20c358a8999237960966e6a252
Shitong Luo <luost26@gmail.com>
2024-10-30 21:54:14 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="efficient_esm",
+    version="0.0.1",
+    packages=["efficient_esm"],
+    package_dir={
+        "efficient_esm": "efficient_esm",
+    },
+)
