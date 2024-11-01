e80914a7fa89520f7c4d5c22c9f7bb1ffd01ead6
Shitong Luo <luost26@gmail.com>
2024-11-01 14:40:40 -0400

    Update

---
+++ data/download_all.sh
@@ -0,0 +1,7 @@
+wget https://dl.fbaipublicfiles.com/fair-esm/models/esm2_t36_3B_UR50D.pt
+wget https://dl.fbaipublicfiles.com/fair-esm/regression/esm2_t36_3B_UR50D-contact-regression.pt
+# wget https://dl.fbaipublicfiles.com/fair-esm/models/esmfold_3B_v1.pt
+# wget https://dl.fbaipublicfiles.com/fair-esm/models/esmfold_structure_module_only_15B.pt
+wget https://dl.fbaipublicfiles.com/fair-esm/models/esmfold_structure_module_only_3B.pt
+wget https://dl.fbaipublicfiles.com/fair-esm/structural-data/splits.tar.gz
+wget https://dl.fbaipublicfiles.com/fair-esm/structural-data/pkl.tar.gz
