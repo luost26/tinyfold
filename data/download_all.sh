6ffd9e49c77d97a83b6de0f0c2a09fc83fa3785d
Yu-Cheng Wu <ycw1031@mit.edu>
2024-11-23 14:54:15 -0500

    Update download script

--- data/download_all.sh
+++ data/download_all.sh
@@ -1,3 +1,5 @@
+pushd $(dirname "$0")
+
 wget https://dl.fbaipublicfiles.com/fair-esm/models/esm2_t36_3B_UR50D.pt
 wget https://dl.fbaipublicfiles.com/fair-esm/regression/esm2_t36_3B_UR50D-contact-regression.pt
 # wget https://dl.fbaipublicfiles.com/fair-esm/models/esmfold_3B_v1.pt
@@ -5,3 +7,5 @@ wget https://dl.fbaipublicfiles.com/fair-esm/regression/esm2_t36_3B_UR50D-contac
 wget https://dl.fbaipublicfiles.com/fair-esm/models/esmfold_structure_module_only_3B.pt
 wget https://dl.fbaipublicfiles.com/fair-esm/structural-data/splits.tar.gz
 wget https://dl.fbaipublicfiles.com/fair-esm/structural-data/pkl.tar.gz
+
+popd
