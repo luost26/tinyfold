56b8ea8416c3cbc17c5c9cae4e4fb73356412b46
Shitong Luo <luost26@gmail.com>
2024-11-30 09:15:40 -0500

    Update create_testset script

--- scripts/create_testset.py
+++ scripts/create_testset.py
@@ -1,3 +1,4 @@
+import random
 import pickle
 import tarfile
 from pathlib import Path
@@ -28,8 +29,10 @@ def get_pickle_path_by_scop_id(scop_id: str) -> str:
     type=click.Path(exists=True, dir_okay=True, path_type=Path),
     default="./data",
 )
-@click.option("--limit", default=100, type=int)
-@click.option("--length-limit", default=384, type=int)
+@click.option("--limit", default=10, type=int)
+@click.option("--seed", default=42, type=int)
+@click.option("--min-length", default=100, type=int)
+@click.option("--max-length", default=400, type=int)
 def main(
     split_by: str,
     split_index: int,
@@ -37,14 +40,19 @@ def main(
     pkl_path: Path,
     output_root: Path,
     limit: int,
-    length_limit: int,
+    seed: int,
+    min_length: int,
+    max_length: int,
 ):
     # Load splits/{split_by}/{split_index}/valid.txt in splits.tar.gz
     with tarfile.open(split_path, "r:gz") as f:
         scop_ids = f.extractfile(f"splits/{split_by}/{split_index}/valid.txt").read().decode().splitlines()
     print("Number of all data points:", len(scop_ids))
     print("Max number of data points to export:", limit)
-    print("Length limit:", length_limit)
+    print(f"Length limit: [{min_length}, {max_length}]")
+
+    scop_ids.sort()
+    random.Random(seed).shuffle(scop_ids)
 
     output_path = output_root / f"testset/{split_by}_{split_index}.pkl"
     outseq_path = output_root / f"testset/{split_by}_{split_index}_sequences.txt"
@@ -60,8 +68,9 @@ def main(
                     pkl_path = get_pickle_path_by_scop_id(scop_id)
                     pkl = f.extractfile(pkl_path).read()
                     data = pickle.loads(pkl)
-                    if len(data["seq"]) > length_limit:
-                        print(f"{scop_id} is too long, ignoring")
+                    seqlen = len(data["seq"])
+                    if seqlen < min_length or seqlen > max_length:
+                        print(f"{scop_id} is too short/long ({seqlen}), ignoring")
                         continue
                     out.append(data)
                     pbar.update(1)
